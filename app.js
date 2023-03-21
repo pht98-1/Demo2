f25957007108f945b2ecf2769e3e8b91d308988a
pht <76780365+pht98-1@users.noreply.github.com>
2023-03-21 08:42:10 +0700

    ok

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ async function deleteProduct(id){
      await dbo.collection('products').deleteOne({_id: new ObjectId(id)})
 }
 
-const PORT =process.env.PORT | 3000
+const PORT =process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log('server is up at ' + PORT)
 })
